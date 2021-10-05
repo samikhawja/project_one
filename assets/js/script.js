d7c50c3bb8357ca32ec9c530aefa663d20918f8d
TZ <tz116917@gmail.com>
2021-10-05 14:31:14 -0700

    js draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("transBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("transBtn");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 
 
 // Activate KanyeQuote when clicked
-btn.addEventListener("click", function(){getKanyeQuote()})
+// btn.addEventListener("click", function(){getKanyeQuote()})
 
 
 function getKanyeQuote(){
@@ -28,21 +28,42 @@ function getKanyeQuote(){
       return response.json()
     })
     .then(function(data){
+      console.log("quote is: " + data.quote)
       modal.textContent= data.quote
+      console.log("quote is: " + data.quote)
       quoteArray=data.quote.split(" ")
       var textInput = ""
       for (i=0;i<quoteArray.length-1;i++){
-        textInput.concat(quoteArray[i]+"%20")
+        textInput = textInput.concat(quoteArray[i]+"%20")
 
       }
-      textInput.concat(quoteArray[quoteArray.length-1])
+      textInput = textInput.concat(quoteArray[quoteArray.length-1])
+      console.log(textInput)
 
-      translateYoda()
+      
+      
+      translate("yoda", textInput)
+      translate("dothraki", textInput)
       
   })
 }
 
+function translate(language, input){
+  var url = "https://api.funtranslations.com/translate/"+language+".json?text="+input+"&api_key=ERi5YlYgaVXbRvTtr08K9AeF"
+  fetch(url)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(data){
+      console.log(data)
+      modal.textContent = data.contents.translated
+      console.log(data.contents.translated)
 
+    })
+}
+
+
+getKanyeQuote()
 
 
 
@@ -77,15 +98,15 @@ function getKanyeQuote(){
 
 ///////////////
 //Kanye API
-function getKanyeQuote () {
-$.ajax({
-  url: kanyeURL,
-  method: "GET"
-}).then(function(response){
-  console.log("this is the kanye quote: ", response);
-  $('#kanyeQuote').text(response);
-})
-}
+// function getKanyeQuote () {
+// $.ajax({
+//   url: kanyeURL,
+//   method: "GET"
+// }).then(function(response){
+//   console.log("this is the kanye quote: ", response);
+//   $('#kanyeQuote').text(response);
+// })
+// }
 // getKanyeQuote();
 // console.log("did it work? " + getKanyeQuote());
 
